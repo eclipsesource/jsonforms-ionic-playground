6aa3b5eb76ba21123f9e74cf2c831f935f824cb5
Edgar Mueller <emueller@eclipsesource.com>
2018-10-17 14:21:03 +0200

    Remove bad import

--- src/app/JsonFormsPage.module.ts
+++ src/app/JsonFormsPage.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import {JsonFormsPage} from "./JsonFormsPage";
 import {IonicPageModule} from "ionic-angular";
-import {IonicSelectableModule} from "ionic-selectable";
 
 @NgModule({
   declarations: [
@@ -9,7 +8,6 @@ import {IonicSelectableModule} from "ionic-selectable";
   ],
   imports: [
     IonicPageModule.forChild(JsonFormsPage),
-    IonicSelectableModule
   ],
   entryComponents: [
     JsonFormsPage
