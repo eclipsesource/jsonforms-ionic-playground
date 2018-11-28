3f54fe698729026d287cdbbe9929331a3022ad1b
Edgar Mueller <emueller@eclipsesource.com>
2018-11-28 14:38:47 +0100

    Configure linter and fix warnings

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,29 +1,30 @@
+// tslint:disable object-literal-sort-keys
 export default {
   orders: [
     {
       customer: {
-        id: '471201',
-        name: 'Sirius Cybernetics Corporation',
-        department: 'Complaints Division'
+        department: "Complaints Division",
+        id: "471201",
+        name: "Sirius Cybernetics Corporation",
       },
-      title: '42 killer robots',
+      title: "42 killer robots",
       ordered: true,
       processId: 144,
-      assignee: 'Philip J. Fry',
-      status: 'ordered',
-      startDate: '2018-06-01',
-      endDate: '2018-08-01',
+      assignee: "Philip J. Fry",
+      status: "ordered",
+      startDate: "2018-06-01",
+      endDate: "2018-08-01",
     },
     {
       customer: {
-        id: '471202',
-        name: 'Very Big Corporation of America'
+        id: "471202",
+        name: "Very Big Corporation of America"
       },
-      title: '1000 gallons of MomCorp Oil',
-      processId: '1890004499',
-      assignee: 'Jen Barber',
-      startDate: '2018-07-01',
-      status: 'planned',
+      title: "1000 gallons of MomCorp Oil",
+      processId: 1890004499,
+      assignee: "Jen Barber",
+      startDate: "2018-07-01",
+      status: "planned",
       amount: 30
     }
   ]
