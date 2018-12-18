84db97f7c5209b340f71e811048dc55dbb62480e
Edgar Mueller <emueller@eclipsesource.com>
2018-12-18 12:42:45 +0100

    Move locale storage into own service, add lodash for cloning schemas

--- src/app/lang.page.ts
+++ src/app/lang.page.ts
@@ -1,7 +1,8 @@
 import {NgRedux} from "@angular-redux/store";
 import {Component} from "@angular/core";
 import {JsonFormsControl} from "@jsonforms/angular";
-import {getLocale, JsonFormsState, setLocale} from "@jsonforms/core";
+import {ControlProps, getLocale, JsonFormsState, setLocale, setSchema, setUISchema} from "@jsonforms/core";
+import {LangService} from "./lang.service";
 
 @Component({
   selector: "lang-page",
@@ -16,15 +17,17 @@ export class LangPage extends JsonFormsControl {
 
   private currentLocale: string;
 
-  constructor(protected ngRedux: NgRedux<JsonFormsState>) {
+  constructor(protected ngRedux: NgRedux<JsonFormsState>, private langService: LangService) {
     super(ngRedux);
   }
 
-  public mapAdditionalProps() {
+  public mapAdditionalProps(props: ControlProps) {
     this.currentLocale = getLocale(this.ngRedux.getState());
   }
 
   private changeLocale(localeString: string) {
     this.ngRedux.dispatch(setLocale(localeString));
+    this.ngRedux.dispatch(setSchema(this.langService.getSchema(localeString)));
+    this.ngRedux.dispatch(setUISchema(this.langService.getUISchema(localeString)));
   }
 }
