d733aaf009739600eee666a061b78dd268ca3c12
Edgar Mueller <emueller@eclipsesource.com>
2019-01-15 14:30:43 +0100

    Add deploy script, update index.html

---
+++ deploy-gh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# based on https://gist.github.com/nicobytes/8c7c0a2139af48b9fc217d107d7e7cc0
+
+git branch -D gh-pages
+git push origin --delete gh-pages
+git checkout -b gh-pages
+ionic build 
+find . -type d ! -path './www*' ! -path './.git*' ! -path '.' | xargs rm -rf
+rm -r  *.*
+mv www/* .
+rm -rf www
+git add .
+git commit -m "Publishing to github pages"
+git push origin gh-pages
+git checkout master
