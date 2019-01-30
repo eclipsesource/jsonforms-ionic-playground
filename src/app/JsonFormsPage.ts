a006b1512735cb8a5771fffc25c70ede46fe4f7d
Edgar Mueller <emueller@eclipsesource.com>
2019-01-30 14:58:12 +0100

    Constraint width, add title

--- src/app/JsonFormsPage.ts
+++ src/app/JsonFormsPage.ts
@@ -4,6 +4,12 @@ import {IonicPage} from "ionic-angular";
 @IonicPage()
 @Component({
   selector: "jsonforms-page",
-  template:  "<jsonforms-outlet></jsonforms-outlet>"
+  template:  `
+  <ion-header>
+    <ion-title>JSON Forms Ionic Playground</ion-title>
+  </ion-header>
+  <ion-content>
+    <jsonforms-outlet></jsonforms-outlet>
+  </ion-content>`
 })
 export class JsonFormsPage { }
